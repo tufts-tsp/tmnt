da560f0fcd9b2e0b3024a52d221a7bd82e8f2b97
zenw00kie <zenw00kie@protonmail.com>
2023-12-28 13:51:09 -0500

    setup info

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(name='bang_pytm',
       version='0.0.0',
       description='Not PyTM',
-      author='TSP',
-      packages=['bang_pytm','bang_pytm.core'])
+      author='Tufts Security & Privacy Lab',
+      packages=['bang_pytm',
+                'bang_pytm.core',
+                'bang_pytm.engine',
+                'bang_pytm.util',
+                'bang_pytm.visual'])
