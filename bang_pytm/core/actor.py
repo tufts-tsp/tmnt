fc1a83d9c19d5e7fd65db2776b7a3c7e995f6875
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2024-01-25 20:44:57 -0500

    actor

--- bang_pytm/core/actor.py
+++ bang_pytm/core/actor.py
@@ -1,7 +1,11 @@
 from .element import Element
 
-
 class Actor(Element):
+    """a person or organization that is represented in the threat model"""
+
+    
+
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
         # TO DO
+    
