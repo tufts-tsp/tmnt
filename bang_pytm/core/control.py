293ad60c89198defe2729b13876b3f3f4f069319
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2024-04-03 10:47:00 -0400

    added control object/control catalog

--- bang_pytm/core/control.py
+++ bang_pytm/core/control.py
@@ -1,11 +1,59 @@
 from .element import Element
 
+#### Based on the OSCAL stuff, is it appropriate to have this Part class here?
+######### How would this work for the init?
+##### I guess a better question would be is this a good way to set it up if we
+##### are parsing an OSCAL catalog??
+class Part:
+    __part_id: str = None
+    __part_name: str = None
+    __part_prose: str = None
+
+    def __init__(self, id: str, prose: str) -> None:
+        self.__part_id = id
+        self.__part_prose = prose
+
+    @property
+    def part_name(self) -> str:
+        return self.__part_name
+    
+    @part_name.setter
+    def part_name(self, name: str) -> None:
+        names = [
+            "objective",
+            "statement",
+            "guidance",
+            "item",
+            "information"
+        ]
+        if name not in names:
+            err = f"Part name must be from following: {','.join(names)}."
+            raise ValueError(err)
+        self.__part_name = name
 
 class Control(Element):
 
+    """
+    A control is a safegaurd or countermeasure prescribed for an information
+    system or an organization to protect the confidentiality, integrity, and
+    availability of the system and its information. In OSCAL, a control is a
+    requirement or guideline, which when implemented will reduce an aspect of
+    risk related to an information system and its information.
+
+    Source: 'KEY CONCEPTS AND TERMS USED IN OSCAL 
+        <https://pages.nist.gov/OSCAL/resources/concepts/terminology/>'
+    """
+
     __id: str = None
     __title: str = None
-    __description: str = None
+
+    # properties have a label and a textual context which defines
+    # the property's value
+    ######### how to init this?????
+    __prop: dict = {}
+
+    # in this case, it must have name statement
+    __part: Part = None
     __assumptions: list = []
     __development_phase: list = []
 
@@ -13,18 +61,25 @@ class Control(Element):
             self, 
             id: str,
             title: str,
+            part: Part,
             description: str
     ):
         self.__id = id
         self.__title = title
+
+        if part.part_name() is not "statement":
+            err = "Name of part must be statement"
+            raise ValueError(err)
+        self.__part = part
+        
         self.__description = description    
 
     @property
-    def assummptions(self) -> list:
+    def assumptions(self) -> list:
         return self.__assumptions
 
-    @assummptions.setter
-    def assummptions(self, assumption_list: list) -> None:
+    @assumptions.setter
+    def assumptions(self, assumption_list: list) -> None:
 
         if not isinstance(assumption_list, list):
             raise ValueError("Assumptions must be provided as a list")
@@ -76,7 +131,7 @@ class Control(Element):
             raise ValueError(err)
         self.__development_phase = phase
 
-
+    
 class ControlCatalog(Element):
     """
     A Control Catalog is a set of controls that are from some standardized set
@@ -84,6 +139,83 @@ class ControlCatalog(Element):
 
     Source: `OSCAL CATALOG <https://pages.nist.gov/OSCAL/resources/concepts/layer/control/catalog/>`
     """
+            
+    class Metadata:
+        """
+        The metadata section of the control catalog contains data about the
+        catalog document. This section has identical structure which is used
+        consistently across all OSCAL models.
 
-    def __init__(self):
-        pass
+        Source: 'CREATING A CONTROL CATALOG <https://pages.nist.gov/OSCAL/learn/tutorials/control/basic-catalog/>'
+        """
+        __title: str = None
+
+        # RFC 3339 format for date/time
+        __published: str = None
+        __last_modified: str = None
+
+        __version: str = None
+        __oscal_version: str = None
+
+        def __init__(self,
+                     title: str, 
+                     published: str, 
+                     last_modified: str, 
+                     version: str, 
+                     oscal_version: str) -> None:
+            
+            self.__title = title
+            self.__published = published
+            self.__last_modified = last_modified
+            self.__version = version
+            self.__oscal_version = version
+        
+    class Group:
+        """
+        An OSCAL catalog allows for the organization of related controls
+        using groups. A catalog group can represent families of controls
+        or other organizational structures, such as sections in a control
+        catalog.
+
+        Source: 'CREATING A CONTROL CATALOG <https://pages.nist.gov/OSCAL/learn/tutorials/control/basic-catalog/>'
+        """
+        __id: str = None
+        __title: str = None
+
+        # properties have a label and a textual context which defines
+        # the property's value
+        ######### how to init this?????
+        __prop: dict = {}
+
+        # list of other Group objects
+        __subgroups: list = []
+        __subgroup_part: Part = None
+        __controls: list[Control] = None
+
+        def __init__(self, 
+                     id: str, 
+                     title: str, 
+                     subgroups: list, 
+                     subgroup_part: Part, 
+                     controls: list[Control]) -> None:
+            
+            self.__id = id
+            self.__title = title
+            self.__subgroups = subgroups
+            self.__subgroup_part = subgroup_part
+            self.__controls = controls
+                
+    
+    __metadata: Metadata = None
+    __groups: list[Group] = []
+    __controls: list[Control] = []
+
+    def __init__(self, 
+                 metadata: Metadata,
+                 groups: list[Group],
+                 controls: list[Control]
+                 ):
+        
+        self.__metadata = metadata
+        self.__groups = groups
+        self.__controls = controls
--- tests.py
+++ tests.py
@@ -35,25 +35,59 @@ class TestTM(unittest.TestCase):
 
 class TestElement(unittest.TestCase):
     
-    def test_parent_child_assignments(self):
+    def test_parent_assignments(self):
 
         elem1 = Element("test1")
         elem2 = Element("test2")
         elem3 = Element("test3")
 
+        # elem1 becomes the child of elem2
+        # elem2 becomes the parent of elem1
         elem1.parent = elem2
 
         # a parent node should not assign itself as a parent
-        # with self.assertRaises(ValueError):
-        #    elem1.parent = elem1
+        with self.assertRaises(AttributeError):
+            elem1.parent = elem1
 
         # a parent node should not assign its child as a parent
-        # with self.assertRaises(ValueError):
-        #    elem2.parent = elem1
+        with self.assertRaises(AttributeError):
+            elem2.parent = elem1
         
         # a node should not overwrite its parent node without removing it
-        # with self.assertRaises(ValueError):
-        #    elem1.parent = elem3
+        with self.assertRaises(ValueError):
+            elem1.parent = elem3
+        
+        # grandparents should not be allowed
+        with self.assertRaises(AttributeError):
+            # elem1 is parent of elem3
+            # elem2 is the parent of elem1
+            # elem2 is the grandparent of elem3
+            elem3.parent = elem1
+    
+    def test_child_assignments(self):
+        
+        elem1 = Element("test1")
+        elem2 = Element("test2")
+        elem3 = Element("test3")
+        elem5 = Element("test5")
+
+        # elem2 is now a child of elem1
+        elem1.add_child(elem2)
+
+        # if child is self.__parent
+        # self.__parent = elem1
+        with self.assertRaises(ValueError):
+            elem2.add_child(elem1)
+        
+        # no grandparents/grandchildren allowed
+        with self.assertRaises(AttributeError):
+            elem3.add_child(elem1)
+
+        # different parent already assigned
+        elem3.add_child(elem5)
+        with self.assertRaises(AttributeError):
+            elem1.add_child(elem5)
+
 
 if __name__ == '__main__':
     unittest.main()
