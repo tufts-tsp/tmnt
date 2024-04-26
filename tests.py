753be6e2666617dcfceec2680c7c1d36e25f27c5
mirarj <44106058+mirarj@users.noreply.github.com>
2024-04-26 14:08:50 -0400

    re added tests

--- tests.py
+++ tests.py
@@ -112,7 +112,6 @@ class TestElement(unittest.TestCase):
 
         # AttributeError: A different parent has already been assigned
         #    elem1.add_child(elem2)
-
     
 class TestComponent(unittest.TestCase):
     def setUp(self):
@@ -169,10 +168,10 @@ class TestComponent(unittest.TestCase):
         self.component.add_data(data)
         self.component.remove_data(data)
 
-    # def tearDown(self):
-    #     self.component.reset()
-    # AttributeError: 'Component' object has no attribute 'reset'
-    # is there supposed to be a reset function
+    def tearDown(self):
+        for c in self.component.controls:
+            self.component.remove_control(c)
+        return super().tearDown()
 
 class TestAsset(unittest.TestCase):
     def setUp(self):
@@ -222,7 +221,199 @@ class TestFlow(unittest.TestCase):
         self.assertEqual(self.flow.authentication, None)
         self.assertTrue(self.flow.multifactor_authentication)
 
+class TestSources(unittest.TestCase):
+    def test_load_asvs(self):
+        asvs = load_owasp_asvs()
+        # assertEqual
+        for c in asvs:
+            self.assertEqual(type(c.id), str)
+            self.assertEqual(type(c.title), str)
+            self.assertEqual(type(c.desc), str)
+            self.assertEqual(type(c.related), list)
+            for id in c.related:
+                self.assertEqual(type(id), dict)
+    def test_load_capec(self):
+        capec = load_capec()
+        for t in capec:
+            self.assertEqual(type(t.name), str)
+            self.assertEqual(type(t.desc), str)
+            self.assertEqual(type(t.prerequisites), list)
+            for p in t.prerequisites:
+                self.assertEqual(type(p), str)
+            self.assertEqual(type(t.mitigations), list)
+            for m in t.mitigations:
+                self.assertEqual(type(m), str)
+            self.assertEqual(type(t.meta['ref_id']), str)
+            self.assertEqual(type(t.meta['long_desc']), str)
+            self.assertEqual(type(t.meta['likelihood']), str)
+            self.assertEqual(type(t.meta['severity']), str)
+            self.assertEqual(type(t.meta['related']), list)
+            for r in t.meta['related']:
+                self.assertEqual(type(r), dict)
+            self.assertEqual(type(t.meta['references']), list)
+            for r in t.meta['references']:
+                self.assertEqual(type(r), dict)
+            self.assertEqual(type(t.consequences), list)
+            for c in t.consequences:
+                self.assertEqual(type(c), dict)
+            self.assertEqual(type(t.threat_source['required_skills']), list)
+            for s in t.threat_source['required_skills']:
+                self.assertEqual(type(s), dict)
+            self.assertEqual(type(t.threat_source['required_resources']), list)
+            for r in t.threat_source['required_resources']:
+                self.assertEqual(type(r), str)
+            self.assertEqual(type(t.examples), list)
+            for e in t.examples:
+                self.assertEqual(type(e), str)
+    def test_load_cwes(self):
+        cwes = load_cwes()
+        for w in cwes:
+            self.assertEqual(type(w.name), str)
+            self.assertEqual(type(w.meta['ref_id']), str)
+            self.assertEqual(type(w.alt_name), list)
+            for a in w.alt_name:
+                self.assertEqual(type(a), str)
+            self.assertEqual(type(w.desc), str)
+            self.assertEqual(type(w.meta['long_desc']), str)
+            self.assertEqual(type(w.modes_of_introduction), list)
+            for m in w.modes_of_introduction:
+                self.assertEqual(type(m), str)
+            self.assertEqual(type(w.meta['likelihood']), str)
+            self.assertEqual(type(w.consequences), list)
+            for c in w.consequences:
+                self.assertEqual(type(c), dict)
+            self.assertEqual(type(w.meta['related']), list)
+            for r in w.meta['related']:
+                self.assertEqual(type(r), dict)
+            self.assertEqual(type(w.mitigations), list)
+            for m in w.mitigations:
+                self.assertEqual(type(m), dict)
+            self.assertEqual(type(w.detection_methods), list)
+            for d in w.detection_methods:
+                self.assertEqual(type(d), dict)
+            self.assertEqual(type(w.meta['references']), list)
+            for r in w.meta['references']:
+                self.assertEqual(type(r), dict)
 
+class TestIssue(unittest.TestCase):
+    def setUp(self):
+        self.issue = Issue("test_issue")
+    def test_add_remove_consequence(self):
+        self.issue.add_consequence("spoofing", "high")
+        self.issue.remove_consequence(1)
+        self.assertEqual(len(self.issue.consequences), 0)
+    def test_add_remove_consequence_multiple(self):
+        self.issue.add_consequence("spoofing", "high")
+        self.issue.add_consequence("spoofing", "high")
+        self.issue.add_consequence("tampering", "low")
+        self.issue.add_consequence("invalid value", "high")
+        self.assertEqual(len(self.issue.consequences), 4)
+        self.issue.remove_consequence(4)
+        self.issue.remove_consequence(2)
+        self.assertEqual(len(self.issue.consequences), 2)
+    def tearDown(self) -> None:
+        for c in self.issue.consequences:
+            self.issue.remove_consequence(c["id"])
+        return super().tearDown()
+
+class TestThreat(unittest.TestCase):
+    def setUp(self):
+        self.threat = Threat("test_threat")
+    def test_add_remove_step(self):
+        self.threat.add_step(1, "Explore")
+        self.threat.remove_step(1)
+    def test_add_remove_step_multiple(self):
+        self.threat.add_step(1, "Explore")
+        self.threat.add_step(2, "Experiment")
+        self.assertEqual(len(self.threat.attack_steps), 2)
+        self.threat.add_step(2, "Explore")
+        self.assertEqual(len(self.threat.attack_steps), 2)
+        self.threat.add_step(5, "Exploit")
+        self.threat.add_step(4, "invalid value")
+        self.threat.remove_step(4)
+        self.assertEqual(len(self.threat.attack_steps), 3)
+        self.threat.remove_step(3)
+        self.assertEqual(len(self.threat.attack_steps), 3)
+    def tearDown(self) -> None:
+        for s in self.threat.attack_steps:
+            self.threat.remove_step(s["order"])
+        return super().tearDown()
+
+class TestWeakness(unittest.TestCase):
+    def setUp(self):
+        self.weakness = Weakness("test_weakness")
+    def test_add_introduction(self):
+        self.weakness.add_introduction("Policy")
+        with self.assertRaises(ValueError):
+            self.weakness.add_introduction("invalid value")
+    def test_add_detection_method(self):
+        self.weakness.add_detection_method("description")
+    def tearDown(self) -> None:
+        self.weakness = None
+        return super().tearDown()
+
+class TestVulnerability(unittest.TestCase):
+    def setUp(self):
+        self.vulnerability = Vulnerability("test_vulnerability")
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+class TestFinding(unittest.TestCase):
+    def setUp(self):
+        self.finding = Finding(affected_components=TestComponent, issues=TestIssue)
+        self.finding = Finding(affected_components=Component("test_component"), issues=Issue("test_issue"))
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+class TestControl(unittest.TestCase):
+    def setUp(self):
+        self.vulnerability = Control("1", "test_control")
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+class TestControlCatalog(unittest.TestCase):
+    def setUp(self):
+        self.control_catalog = ControlCatalog()
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+class TestActor(unittest.TestCase):
+    def setUp(self):
+        self.actor = Actor("test_actor")
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+class TestThreatlib(unittest.TestCase):
+    pytm_rules = Rules()
+    tm_components = [Asset("A"), Asset("B"), Process("C")]
+    findings = get_findings(tm_components, pytm_rules)
+    def setUp(self):
+        self.rules = Rules()
+        asset1 = Asset("A")
+        asset2 = Asset("B")
+        process1 = Process("C")
+        process2 = Process("D")
+        datastore1 = Datastore("E")
+        datastore2 = Datastore("F")
+        external1 = ExternalEntity("G")
+        external2 = ExternalEntity("H")
+        dataflow1 = DataFlow("I", src=Element("s"), dst=Element("d"))
+        dataflow2 = DataFlow("J", src=Element("s"), dst=Element("d"))
+        self.tm_components = [asset1, asset2, process1, process2, datastore1, datastore2, external1, external2, dataflow1, dataflow2]
+        for i in range(1,10,2):
+            for r in self.rules.threatmap:
+                for c in r.controls:
+                    if c.title not in [x.title for x in self.tm_components[i].controls]:
+                        self.tm_components[i].add_control(c)
+    
+    def test_findings(self):
+        self.findings = get_findings(self.tm_components, self.rules)
+
+    def tearDown(self) -> None:
+        for component in self.tm_components:
+            for control in component.controls:
+                component.remove_control(control)
+        return super().tearDown()
 
 if __name__ == '__main__':
     unittest.main()
