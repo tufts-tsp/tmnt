669a5f21d97617c1a0ed086c1c41b5c56dd877cf
Ron Thompson <zenw00kie@protonmail.com>
2024-05-10 14:09:07 -0400

    reference fix

--- setup.py
+++ setup.py
@@ -2,19 +2,10 @@ from setuptools import setup
 
 setup(name='tmnt_dsl',
       version='0.0.0',
-      description='Not PyTM',
+      description='DSL for the Threat Modeling Naturally Tool',
       author='Tufts Security & Privacy Lab',
       packages=['tmnt_dsl',
                 'tmnt_dsl.core',
-                'tmnt_dsl.engine',
                 'tmnt_dsl.util',
                 'tmnt_dsl.visual'],
-      package_data={
-          "tmnt_dsl.util":[
-              "reference_data/cwe.xml",
-              "reference_data/capec.xml",
-              "reference_data/asvs.xml",
-              "reference_data/asvs_ref.json",
-          ]
-      }
       )
