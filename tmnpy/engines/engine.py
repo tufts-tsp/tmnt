ff4b8c9644fa85826fc18001c045affcd39e2ec2
Ron Thompson <zenw00kie@protonmail.com>
2024-06-19 13:34:22 -0400

    keeping in line with rest of tmnpy
    Same property methods as `tmnpy.dsl.Element`

--- tmnpy/engines/engine.py
+++ tmnpy/engines/engine.py
@@ -1,13 +1,14 @@
 from enum import Enum
 from uuid import uuid4, UUID
 import time
+
 class Engine(object):
     def __init__(
         self,
         name: str,
         desc: str = "N/A",
     ):
-        self.__id = uuid.uuid4()
+        self.__id = uuid4()
         if not isinstance(name, str):
             raise ValueError("Engine Name must be a string")
         self.__name = name
@@ -17,7 +18,29 @@ class Engine(object):
             raise ValueError("Engine Description must be a string")
         self.__desc = desc
 
-class Event_Type(Enum):
+    @property
+    def eid(self) -> UUID:
+        """Unique ID for the Element"""
+        return self.__id
+
+    @property
+    def name(self) -> str:
+        """Name of the Element"""
+        return self.__name
+
+    @name.setter
+    def name(self, val: str) -> None:
+        self.__name = val
+
+    @property
+    def desc(self) -> str:
+        """Description of the Element"""
+        return self.__desc
+
+    @desc.setter
+    def desc(self, val: str) -> None:
+        self.__desc = val
+
 class EventType(Enum):
 
     """
@@ -42,7 +65,7 @@ class NaturalEngine(Engine):
         self.previous_events = []
         self.lastevent = time.time()
         super().__init__(name,desc)
-        
+
     def event(self, event_type: str):
     # Get the event and check what type it is, then plug into markov model to predict new focus.  Update list of previous events to include this.
         self.previous_events.append(event_type)
