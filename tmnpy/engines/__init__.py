c7c6e34c96d50eeaa4c1cf1f3d56e5cd1c3c0cb5
Ron Thompson <zenw00kie@protonmail.com>
2024-06-19 13:44:51 -0400

    ensure availability of NaturalEngine
    such that the user can call `tmnpy.engines.NaturalEngine`

--- tmnpy/engines/__init__.py
+++ tmnpy/engines/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["Engine", "Assignment"]
+__all__ = ["Engine", "NaturalEngine", "Assignment"]
 
-from .engine import Engine
+from .engine import Engine, NaturalEngine
 from .assignment import Assignment
