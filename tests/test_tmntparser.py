f453e6a961538d58c59a679c769466b2270cd42d
Yaejie Lee Kwon <yaejiekwon@Yaejies-MacBook-Pro.local>
2024-06-27 15:18:29 -0400

    tmnt parser test changes

--- tests/test_tmntparser.py
+++ tests/test_tmntparser.py
@@ -1,119 +1,26 @@
 import unittest
 
-from tmnpy.util.tmntparser import TMNTParser
-from tmnpy.dsl import *
+from tmnpy.util.parsers import TMNTParser
+from tmnpy.dsl.asset import Machine
+from tmnpy.dsl.requirement import SecurityProperty, Property
+
 
 
 
 class TestTMNTParser(unittest.TestCase):
 
-    def __init__(self):
-        self.parser = TMNTParser(name="threatmodel_test", "../examples/parser_examples/presentation_dsl_example.yaml")
+    def setUp(self):
+        self.parser = TMNTParser(tm_name = "threatmodel_test", yaml = "../examples/parser_examples/presentation_dsl_example.yaml")
         self.TM = self.parser.tm
         return super().setUp()
 
 
-    #parse workflow
-    def test_parse_workflow(self):
-
-
-    #parse dataflow
-    def test_parse_dataflow(self):
-
-
-    def test_parse_flow(self):
-        flow_data = self.example_data["flow"]
-        flow = self.parser.parse_flow(flow_data)
-
-        name = flow.name
-        authentication = flow.authentication
-        multifactor_authentication = flow.multifactor_authentication
-
-        self.assertEqual(name, "Data Transfer from Life Support to Surgeon Workstation")
-        self.assertEqual(authentication, "TLS")
-        self.assertEqual(multifactor_authentication, false)
-
-
-
-    #process
-    def test_parse_process(self):
-
-
-    #datastore
-    def test_parse_datastore(self):
-
-
-    def test_parse_asset(self):
-        asset_data = self.example_data["assets"]
-        asset = self.parser.parse_asset(asset_data)
-
-        name = asset_data.name
-        machine = asset_data.machine
-
-        self.assertEqual(name, "Life Support/Monitoring Equipment")
-        self.assertEqual(machine, "PHYSICAL")
-
-
-    def test_parse_external_entities(self):
-        ee_data = self.example_data["external_entitie"]
-        external_entities = self.parser.pparse_external_entitie(ee_data)
-
-        name = external_entities.name
-        physical_access = external_entities.physical_access
-
-        self.assertEqual(name, "Remote Maintenance Provider")
-        self.assertEqual(physical_access, false)
-
-
-    #weakness
-    def test_parse_weakness(self):
-
-
-    #vulnerability
-    def test_parse_vulnerability(self):
-
-
-    #threat
-    def test_parse_threat(self):
-
-
-    #issue
-    def test_parse_issue(self):
-
-
-    def test_parse_data(self):
-        for item in self.example_data:
-            print(item)
-            for obj in self.example_data[item]:
-                print(obj)
-
-
-        data = self.parser.parse_data(d_data)
-
-        name = data.name
-        is_pii = data.is_pii
-        is_phi = data.is_phi
-        format = data.format
-        is_credentials = data.is_credentials
-        lifetime = data.lifetime
-
-
-        self.assertEqual(name, "Patient Monitoring Data")
-        self.assertEqual(is_pii, true)
-        self.assertEqual(is_phi, true)
-        self.assertEqual(format, "Digital")
-        self.assertEqual(is_credentials, false)
-        self.assertEqual(lifetime, "For the duration of the surgery")
-
-
-
-    #component
-    def test_parse_component(self):
-
-
     def test_parse_actor(self):
-        actor_data = self.example_data["actor"]
-        actor = self.parser.parse_actor(actor_data)
+        actor_data = [
+            a for a in self.TM.actors if a.name == "Surgeon"
+        ]
+        if len(actor_data) == 1:
+            actor = actor_data[0]
 
         name = actor.name
         actor_type = actor.actor_type
@@ -121,49 +28,193 @@ class TestTMNTParser(unittest.TestCase):
 
         self.assertEqual(name, "Surgeon")
         self.assertEqual(actor_type, "Individual")
-        self.assertEqual(physical_access, true)
+        self.assertEqual(physical_access, True)
 
 
-    #boundary
-    def test_parse_boundary(self):
+    def test_parse_asset(self):
+        asset_data = [
+            a for a in self.TM.components if a.name == "Life Support/Monitoring Equipment"
+        ]
+        if len(asset_data) == 1:
+            asset = asset_data[0]
+
+        name = asset.name
+        security_property = asset.security_property
+        machine = asset.machine
+        data = asset.data
+
+        self.assertEqual(name, "Life Support/Monitoring Equipment")
+        self.assertEqual(security_property.confidentiality, Property.HIGH)
+        self.assertEqual(security_property.integrity, Property.HIGH)
+        self.assertEqual(security_property.availability, Property.HIGH)
+        self.assertEqual(machine, Machine.PHYSICAL)
+        self.assertEqual(data[0].name, "Patient Monitoring Data")
+        self.assertEqual(data.is_pii, True)
+        self.assertEqual(data.is_phi, True)
+        self.assertEqual(data.format, "Digital")
+        self.assertEqual(data.is_credentials, False)
+        self.assertEqual(data.desc, "Kept for the duration of the surgery")
+        self.assertEqual(data.lifetime, "AUTO")
 
 
-    #element
-    def test_parse_element(self):
+#     def test_parse_external_entities(self):
+#         external_entities_data = self.TM.assets()
+        # if len(external_entities_data) == 1:
+        #     external_entities = external_entities_data[0]
+#         name = external_entities.name
+#         security_property = external_entities.security_property
+#         physical_access = external_entities.physical_access
+#         machine = external_entities.machine
+#         data = external_entities.data
+
+#         self.assertEqual(name, "Remote Maintenance Provider")
+#         self.assertEqual(security_property.confidentiality, "HIGH")
+#         self.assertEqual(security_property.integrity, "HIGH")
+#         self.assertEqual(security_property.availability, "HIGH")
+#         self.assertEqual(physical_access, False)
+#         self.assertEqual(machine, "VIRTUAL")
+#         self.assertEqual(data.name, "Robot Maintenance Logs")
+#         self.assertEqual(data.is_pii, False)
+#         self.assertEqual(data.is_phi, False)
+#         self.assertEqual(data.format, "Textual Log Files")
+#         self.assertEqual(data.is_credentials, False)
+#         self.assertEqual(data.desc, "Retained according to maintenance schedule")
+#         self.assertEqual(data.lifetime, "AUTO")
 
 
-    #safetyimpact
-    def test_parse_safetyimpact(self):
+#     def test_parse_dataflow(self):
+#         dataflow_data = self.TM.flows()
+        # if len(dataflow_data) == 1:
+        #     dataflow = dataflow_data[0]
+
+#         name = dataflow.name
+#         src = dataflow.src
+#         dst = dataflow.dst
+#         port = dataflow.port
+#         protocol = dataflow.protocol
+#         authentication = dataflow.authentication
+#         multifactor_authentication = dataflow.multifactor_authentication
+
+#         self.assertEqual(name, "Data Transfer from Life Support to Surgeon Workstation")
+#         self.assertEqual(src.name, "Life Support/Monitoring Equipment")
+#         self.assertEqual(dst.name, "Surgeon Workstation")
+#         self.assertEqual(port, "443")
+#         self.assertEqual(protocol, "HTTPS")
+#         self.assertEqual(authentication, "TLS")
+#         self.assertEqual(multifactor_authentication, False)
 
 
-    def test_parse_security_property(self):
-        security_property_data = self.TM.components()
-        #["security_property"]
-        security_property = self.parser.parse_security_property(security_property_data)
+#     def test_parse_workflow(self):
+#         workflow_data = self.TM.flows()
+        # if len(workflow_data) == 1:
+        #     workflow = workflow_data[0]
+            
+#         name = workflow.name
+#         src = workflow.src
+#         dst = workflow.dst
+#         path = workflow.path
+#         authentication = workflow.authentication
+#         multifactor_authentication = workflow.multifactor_authentication
 
-        confidentiality = security_property.confidentiality
-        integrity = security_property.integrity
-        availability = security_property.availability
-
-        self.assertEqual(confidentiality, "HIGH")
-        self.assertEqual(integrity, "HIGH")
-        self.assertEqual(availability, "HIGH")
-
-
-
-    #stride
-    def test_parse_stride(self):
-
-
-    #finding
-    def test_parse_finding(self):
+#         self.assertEqual(name, "Surgical Procedure Execution Flow")
+#         self.assertEqual(src.name, "Surgeon Workstation")
+#         self.assertEqual(dst.name, "Surgical Robot")
+#         self.assertEqual(path.name, "Hospital Network")
+#         self.assertEqual(authentication, "Certificate-based")
+#         self.assertEqual(multifactor_authentication, True)
 
 
-    def tearDown(self) -> None:
-        return super().tearDown()
+#     def tearDown(self) -> None:
+#         return super().tearDown()
+
+
+
+# class TestTMNTParser1(unittest.TestCase):
+
+#     def setUp(self):
+#         self.parser = TMNTParser(tm_name = "threatmodel_test", yaml = "dsl_test_example.yaml")
+#         self.TM = self.parser.tm
+#         return super().setUp()
+
+
+#     def test_parse_element(self):
+#         element_data = [
+#             a for a in self.TM.element if a.name == "element example"
+#         ]
+        # if len(element_data) == 1:
+        #     element = element_data[0]
+        
+        # name = element.name
+        # desc = element.desc
+        # parent = element.parent
+        # children = element.children
+        # security_property  = element.security_property
+
+        # self.assertEqual(name, "element example")
+        # self.assertEqual(desc, "element desc")
+        # self.assertEqual(parent.name, "parent example")
+        # self.assertEqual(children.name, "children example")
+#         self.assertEqual(security_property.confidentiality, "HIGH")
+#         self.assertEqual(security_property.integrity, "HIGH")
+#         self.assertEqual(security_property.availability, "HIGH")
+
+
+
+#     def test_parse_boundary(self):
+        # boundary_data = [
+        #     a for a in self.TM.boundaries if a.name == "boundary example"
+        # ]
+#         if len(boundary_data) == 1:
+#           boundary = boundary_data[0]
+
+#         name = boundary.name
+#         boundary_owner = boundary.boundary_owner
+
+#         self.assertEqual(name, "boundary example")
+#         self.assertEqual(boundary_owner, "Actor")
+
+
+
+#     def test_parse_component(self):
+#         component_data = self.TM.components()
+#         if len(component_data) == 1:
+#           component = component_data[0]
+
+#         name = component.name
+
+#         self.assertEqual(name, "component example")
+
+    
+#     def test_parse_process(self):
+#         process_data = self.TM.assets()
+#         if len(process_data) == 1:
+#           process = process_data[0]
+
+#         name = process.name
+
+#         self.assertEqual("process example")
+
+
+#     def test_parse_datastore(self):
+#         datastore_data = self.TM.assets()
+#         if len(datastore_data) == 1:
+#           datastore = datastore_data[0]
+
+#         name = datastore.name
+#         ds_type = datastore.ds_type
+
+#         self.assertEqual("process example")
+#         self.assertEqual("UNKNOWN")
+
+
+#     def tearDown(self) -> None:
+#         return super().tearDown()
 
 
 if __name__ == "__main__":
+    unittest.main()
 
-    parser = TestTMNTParser()
-    parser.test_parse_data()
+
+
+
+# others: finding, stride, property, safetyimpact, issue, threat, vulnerability, weakness
--- tmnpy/dsl/asset.py
+++ tmnpy/dsl/asset.py
@@ -110,8 +110,20 @@ class Datastore(Asset):
     """
 
     def __init__(
-        self, name, ds_type: DATASTORE_TYPE = DATASTORE_TYPE.UNKNOWN, **kwargs
+        self, 
+        name, 
+        # machine: Machine | str = Machine.NA,
+        ds_type: DATASTORE_TYPE | str = DATASTORE_TYPE.UNKNOWN, 
+        **kwargs
     ):
+
+        if isinstance(ds_type, str):
+            ds_type = DATASTORE_TYPE[ds_type]
+
+        if not isinstance(ds_type, DATASTORE_TYPE):
+            raise ValueError("DATASTORE_TYPE must be a DATASTORE_TYPE")
+        self.ds_type = ds_type
+        
         if not isinstance(ds_type, DATASTORE_TYPE):
             raise ValueError("DS Type must be a DATASTORE_TYPE object")
         self.ds_type = ds_type
@@ -119,6 +131,7 @@ class Datastore(Asset):
             raise AttributeError(
                 "If specifying OTHER for the datastore type please include a description."
             )
+
         super().__init__(name, **kwargs)
 
 
