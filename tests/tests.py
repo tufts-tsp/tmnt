e9589f450722e5a7378bb34312190a0905b1bc07
Ron Thompson <zenw00kie@protonmail.com>
2024-07-05 15:24:43 -0400

    fixed tests
    and added parser test to suite

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,8 @@
 # from .test_examples import TestHotelBookingWebsiteExample, TestInsulinDeliveryDeviceExample
 # from .test_oscal_parser import TestOSCALParser
-from tests_engines import TestThreatlib
-from tests_kb import TestASVS, TestCWES, TestCAPEC
-from tests_dsl import (
+from .tests_engines import TestThreatlib
+from .tests_kb import TestASVS, TestCWES, TestCAPEC
+from .tests_dsl import (
     TestTM,
     TestElement,
     TestComponent,
